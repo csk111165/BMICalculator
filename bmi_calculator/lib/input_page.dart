69b08068e35b20790007804876b937847394c0bd
Chandra Shekhar <cshekhar@nvidia.com>
2023-08-20 02:23:48 +0530

    Adding the skelton UI

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -12,9 +12,86 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Center(child: Text('BMI CALCULATOR')),
       ),
-      body: Center(
-        child: Text('Body Text here'),
-      ),
+      body: Column(children: [
+        Expanded(
+            child: Row(children: [
+          Expanded(
+            child: Container(
+              // for magin
+              margin: EdgeInsets.all(15),
+
+              // for border radius
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                // when we define the BoxDecoration the color property should be inside here only
+                color: Color(0xFF1D1E33),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              // for magin
+              margin: EdgeInsets.all(15),
+
+              // for border radius
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                // when we define the BoxDecoration the color property should be inside here only
+                color: Color(0xFF1D1E33),
+              ),
+            ),
+          ),
+        ])),
+
+        // this is mid part of the screen having only one widget
+        Expanded(
+          child: Container(
+            // for magin
+            margin: EdgeInsets.all(15),
+
+            // for border radius
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              // when we define the BoxDecoration the color property should be inside here only
+              color: Color(0xFF1D1E33),
+            ),
+          ),
+        ),
+
+        // bottom part contains two row elements
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  // for magin
+                  margin: EdgeInsets.all(15),
+
+                  // for border radius
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    // when we define the BoxDecoration the color property should be inside here only
+                    color: Color(0xFF1D1E33),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  // for magin
+                  margin: EdgeInsets.all(15),
+
+                  // for border radius
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    // when we define the BoxDecoration the color property should be inside here only
+                    color: Color(0xFF1D1E33),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
