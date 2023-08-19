08218c18072e4679e5a2d71eab6cbcfba449a68d
Chandra Shekhar <cshekhar@nvidia.com>
2023-08-20 01:15:52 +0530

    Adding input_page.dart file for refactoring code

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -15,32 +16,6 @@ class BMICalculator extends StatelessWidget {
           backgroundColor: Color.fromARGB(255, 139, 153, 226),
         ),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
-      ),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('BMI CALCULATOR')),
-      ),
-      body: Center(
-        child: Text('Body Text here'),
-      ),
-      floatingActionButton: Theme(
-        data: ThemeData.light(),
-        child: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {},
-        ),
       ),
     );
   }
